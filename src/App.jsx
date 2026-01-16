c755645a9413e9dbadba63313d15e31fd27753a1
Henry Bondah <bondahdean@gmail.com>
2026-01-16 16:52:46 -0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { authService } from './utils/storage';
 import Home from './pages/Home';
 import AboutPage from './pages/AboutPage';
